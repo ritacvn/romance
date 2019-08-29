58395b8010787d9d71d4abac69dc7f7a4f35c697
Rita de Cássia <ritadecassia.vasconcelos@hotmail.com>
2019-08-28 22:14:18 -0400

    Added a link from the storyboard text field to the code

--- Controller/InserirNomesViewController.swift
+++ Controller/InserirNomesViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    public var name1var: String = " "
+    public var name2var: String = " "
+    
+    @IBOutlet weak var name1: UITextField!
+    
+    @IBOutlet weak var name2: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+   
 }
 
