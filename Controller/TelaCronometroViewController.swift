76bd6f8a2840106574b5813d86dfc6415383ae78
Rita de Cássia <ritadecassia.vasconcelos@hotmail.com>
2019-08-29 12:02:41 -0400

    Script of stopwatch working fine

--- Controller/TelaCronometroViewController.swift
+++ Controller/TelaCronometroViewController.swift
@@ -7,9 +7,21 @@
 //
 
 import UIKit
-
+import AudioToolbox
 class TelaCronometroViewController: UIViewController {
-
+    
+    var count = 0
+    
+    var minute = 0
+    
+    var hour = 0
+    
+    var timer = Timer()
+    
+    @IBOutlet weak var minuteLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    
+    @IBOutlet weak var acabouLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +29,48 @@ class TelaCronometroViewController: UIViewController {
     }
     
     @IBAction func startButton(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 0.8, target: self, selector: #selector(TelaCronometroViewController.counter), userInfo: nil, repeats: true)
+    }
+    @objc func counter(){
+        count += 1
+        
+        if count >= 0  {
+            secondLabel.text = "\(count)"
+            if count == 60 {
+                count = 0
+                secondLabel.text = "00"
+                print("\(secondLabel.text)")
+                minute += 1
+                if minute > 9 {
+                    minuteLabel.text = "\(minute)"
+                    if minute == 60 {
+                        minute = 0
+                        minuteLabel.text = "00"
+                    }
+                }
+                else{
+                    minuteLabel.text = "0\(minute)"
+                }
+                
+                
+            }
+    }
+        if minuteLabel.text == "01" && secondLabel.text == "00"{
+            acabouLabel.text = "CABBOU MEU FILHO"
+            let generator = UIImpactFeedbackGenerator(style: .heavy)
+            generator.impactOccurred()
+        }
+    }
+    @IBAction func pauseButton(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    @IBAction func resetButton(_ sender: Any) {
+        timer.invalidate()
+        count = 0
+        minute = 0
+        minuteLabel.text = "00"
+        secondLabel.text = "00"
     }
     
 }
