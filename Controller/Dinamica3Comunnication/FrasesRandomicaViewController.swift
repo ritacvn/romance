723d2d39c88f43ef0b8f6d7e0bfd17d7d8d23dfc
Eliza Carvalho <elizampcarvalho@gmail.com>
2019-09-13 09:12:03 -0400

    Added start button to "DinamicaDasFrases"

--- Controller/Dinamica3Comunnication/FrasesRandomicaViewController.swift
+++ Controller/Dinamica3Comunnication/FrasesRandomicaViewController.swift
@@ -9,28 +9,70 @@
 import UIKit
 
 class FrasesRandomicaViewController: UIViewController {
+   
+//MARK: Varibles and Constants
+    
     var frase: Int = 0
+    
+    //time variables
+    var count: Int = 0
+    var minute: Int = 0
+    var second: Int = 0
+    
+//MARK: Outlets
     
     @IBOutlet weak var fraseRandomizada: UILabel!
     
+    //Time labels
     @IBOutlet weak var minuteLabel: UILabel!
-    
     @IBOutlet weak var secondLabel: UILabel!
     
+    
+    
+   
+    
+//MARK: Actions
+    
+    @IBAction func startActivity(_ sender: Any) {
+    }
+    
+
+//MARK: Functions
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       
+        randomizarFrase()
+        
     }
     
-    @IBAction func randomizarFraseButton(_ sender: Any) {
-        randomizarFrase()
-    }
+    //Phrase to randomize phrase
     func randomizarFrase(){
         frase = Int.random(in: 0...3)
         fraseRandomizada.text = arrayDeFrases[frase]
     }
-
     
+    //Time Logic
+    @objc func counter(){
+        count += 1
+        if count >= 0 {
+            secondLabel.text = "0\(count)"
+            if count == 60 {
+                count = 0
+                secondLabel.text = "00"
+                minute += 1
+                if minute >= 0 {
+                    minuteLabel.text = "\(minute)"
+                    if minute == 60 {
+                        minute = 0
+                        minuteLabel.text = "00"
+                    }
+                }
+                else{
+                    minuteLabel.text = "0\(minute)"
+                }
+            }
+        }
+    }
 
 }
