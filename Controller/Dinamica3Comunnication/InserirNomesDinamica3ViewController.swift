1855680717994ce91b52e995829bb94579a16276
Rita de Cássia <ritadecassia.vasconcelos@hotmail.com>
2019-09-13 08:53:34 -0400

    Added text fields outlets to the code
    Signed-off-by: Rita de Cássia <ritadecassia.vasconcelos@hotmail.com>

---
+++ Controller/Dinamica3Comunnication/InserirNomesDinamica3ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  InserirNomesDinamica3ViewController.swift
+//  romance
+//
+//  Created by Rita de Cássia Vasconcelos do Nascimento on 13/09/19.
+//  Copyright © 2019 Rita de Cássia Vasconcelos do Nascimento. All rights reserved.
+//
+
+import UIKit
+
+class InserirNomesDinamica3ViewController: UIViewController {
+
+    @IBOutlet weak var nome1TextField: UITextField!
+    
+    @IBOutlet weak var nome2TextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonNextView(_ sender: Any) {
+    }
+    
+}
