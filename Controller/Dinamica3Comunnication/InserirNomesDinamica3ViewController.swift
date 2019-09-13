290f251f5e949d2d90c76dc25efca8ada285e397
Rita de Cássia <ritadecassia.vasconcelos@hotmail.com>
2019-09-13 09:02:55 -0400

    Dismissing keyboard when touche the screen
    Signed-off-by: Rita de Cássia <ritadecassia.vasconcelos@hotmail.com>

--- Controller/Dinamica3Comunnication/InserirNomesDinamica3ViewController.swift
+++ Controller/Dinamica3Comunnication/InserirNomesDinamica3ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class InserirNomesDinamica3ViewController: UIViewController {
+class InserirNomesDinamica3ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var nome1TextField: UITextField!
     
@@ -16,11 +16,33 @@ class InserirNomesDinamica3ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+        
+        view.addGestureRecognizer(tap)
+        
+        self.nome1TextField.delegate = self
+        self.nome2TextField.delegate = self
+      
     }
     
     @IBAction func buttonNextView(_ sender: Any) {
+        if nome1TextField.text == "" || nome2TextField.text == ""{
+            
+            let message: String = "Desculpe, você não preencheu todos os campos"
+            let alert = UIAlertController(title:title,message: message, preferredStyle: .alert)
+            let action = UIAlertAction(title: "Ok",style: .default, handler: nil)
+            
+            alert.addAction(action)
+            
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
     
 }
