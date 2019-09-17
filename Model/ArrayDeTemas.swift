1887f95c71034f97f530157690ac41481aa23493
Eliza Carvalho <elizampcarvalho@gmail.com>
2019-09-17 10:36:16 -0400

    Added cards to arrayOFcards

--- Model/ArrayDeTemas.swift
+++ Model/ArrayDeTemas.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 let arrayDeTemas_Dinamica03 = ["tema1","tema2","tema3","tema4","tema4","tema5"]
-let arrayImagens: [UIImage] = [UIImage(named: "FrontOfCard_A")!, UIImage(named: "FrontOfCard_B")!]
+let arrayImagens: [UIImage] = [UIImage(named: "CardA")!, UIImage(named: "CardE")!, UIImage(named: "CardI")!,UIImage(named: "CardO")!, UIImage(named: "CardU")!]
+
 let arrayDeFrases = ["frase1","frase2","frase3","frase4"]
-let arrayImages: [UIImage] = [UIImage(named: "communication")!, UIImage(named: "SquareDIN1")!]
+
+//let arrayImages: [UIImage] = [UIImage(named: "communication")!, UIImage(named: "SquareDIN1")!]
+
 let arrarCategorias = ["communication","carinho", "conhecimento","empatia"]
