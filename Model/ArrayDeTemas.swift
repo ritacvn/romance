930d8badf553732c282dd7938990c29c9777368e
Rita <ritadecassia.vasconcelos@hotmail.com>
2019-09-20 00:06:37 -0400

    added real themes to the project

--- Model/ArrayDeTemas.swift
+++ Model/ArrayDeTemas.swift
@@ -8,10 +8,10 @@
 
 import UIKit
 
-let arrayDeTemas_Dinamica03 = ["tema1","tema2","tema3","tema4","tema4","tema5"]
+let arrayDeTemas_Dinamica03 = ["Love","Communication","Dishes","Affection","Jealous","Trust"]
 let arrayImagens: [UIImage] = [UIImage(named: "CardA")!, UIImage(named: "CardE")!, UIImage(named: "CardI")!,UIImage(named: "CardO")!, UIImage(named: "CardU")!]
 
-let arrayDeFrases = ["frase1","frase2","frase3","frase4"]
+let arrayDeFrases = ["Love is not everthing, we have another needs","We always need to be prepared for a break up","Trust is everthing","We need first to be happy alone to than be happy with each other"]
 
 //let arrayImages: [UIImage] = [UIImage(named: "communication")!, UIImage(named: "SquareDIN1")!]
 
