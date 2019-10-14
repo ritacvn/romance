803908770c0e08c29b5c981ae3d3f9ad87bfbbdc
Rita de Cássia <ritadecassia.vasconcelos@hotmail.com>
2019-10-14 08:57:13 -0400

    Added circular progress view pod and added on the screen
    Signed-off-by: Rita de Cássia <ritadecassia.vasconcelos@hotmail.com>

--- Model/PlayScreenViewController.swift
+++ Model/PlayScreenViewController.swift
@@ -7,18 +7,34 @@
 //
 
 import UIKit
+import MBCircularProgressBar
 
 class PlayScreenViewController: UIViewController {
     
     var time: String?
+    var timeConverted: Int?
+    var timer = Timer()
+    var minute: Int = 0
+    var second: Int = 0
     
     @IBOutlet weak var labelTime: UILabel!
+    
+    @IBOutlet weak var progressView: MBCircularProgressBarView!
+    
+    @IBOutlet weak var clockLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         labelTime.text = time
-       
+        timeConverted = Int(time!) ?? 0
+        self.progressView.value = 0
     }
     
+   
+    override func viewWillAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 1.0) {
+            self.progressView.value = 80
+        }
+    }
 
 
 }
