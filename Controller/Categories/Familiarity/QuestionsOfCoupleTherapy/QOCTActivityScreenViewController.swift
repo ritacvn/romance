7ccc36e67159f9aba73ba96e3bd62de10f5eddca
Eliza Carvalho <elizampcarvalho@gmail.com>
2019-10-22 11:33:07 -0400

    Added timer to activity "QuestionsOfCoupleTherapy"

--- Controller/Categories/Familiarity/QuestionsOfCoupleTherapy/QOCTActivityScreenViewController.swift
+++ Controller/Categories/Familiarity/QuestionsOfCoupleTherapy/QOCTActivityScreenViewController.swift
@@ -13,13 +13,20 @@ class QOCTActivityScreenViewController: UIViewController{
     
     var theme: String = ""
     
+    //Time Variables
+    var timer = Timer()
+    var time: Int = 60
+    var count: Int = 0
+    var minute: Int = 0
+    var second: Int = 0
+    
     @IBOutlet weak var mainThemeOutlet: UILabel!
+    @IBOutlet weak var timeOutlet: UILabel!
     
     override func viewDidLoad() {
         updadeTheme()
         mainThemeOutlet.text = "\(String(describing: theme))"
     }
-    
     
     //Randomizing the theme
     func updadeTheme(){
@@ -27,4 +34,53 @@ class QOCTActivityScreenViewController: UIViewController{
         theme = arrayOfQuests_Act1F[randomTheme]
     }
        
+    @IBAction func startButton(_ sender: Any) {
+        
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.8, target: self, selector: #selector(TelaCronometroViewController.counter), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func counter(){
+
+         minute =  time/60
+         second = time%60
+         
+         if time >= 70{
+             
+             timeOutlet.text = "\(String(minute)):\(String(second))"
+             time -= 1
+         }
+             
+         else if time > 60{
+             timeOutlet.text = "0\(String(minute)):0\(String(second))"
+             time -= 1
+         }
+             
+         else if time == 60{
+             timeOutlet.text = "01:00"
+             time -= 1
+         }
+         else if time < 60 && time >= 10{
+             timeOutlet.text = "00:\(String(second))"
+             // timerLabel.text = String(time)
+             time -= 1
+         }
+         else if time < 10{
+             timeOutlet.text = "00:0\(String(second))"
+             time -= 1
+         }
+         if time < 0{
+             
+             self.timer.invalidate()
+             
+         }
+         if timeOutlet.text == "00:00" {
+             timer.invalidate()
+         }
+        
+     }
+    
+    
+    
 }
