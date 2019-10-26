622f665c36bc6a0583a445ef4fd077e74ed97908
Rita de Cássia <ritadecassia.vasconcelos@hotmail.com>
2019-10-26 16:28:05 -0400

    Fiexd circular progress bar in the sound of silence activity
    Signed-off-by: Rita de Cássia <ritadecassia.vasconcelos@hotmail.com>

--- Model/PlayScreenViewController.swift
+++ Model/PlayScreenViewController.swift
@@ -15,64 +15,50 @@ class PlayScreenViewController: UIViewController {
     var minute: Int = 0
     var second: Int = 0
     var time: String?
-    
-
+    var timeLeft: TimeInterval?
+    var timer = Timer()
+    var endTime: Date?
     @IBOutlet weak var labelTime: UILabel!
     @IBOutlet weak var clockLabel: UILabel!
     
     @IBOutlet weak var timeLabel: UILabel!
     let timeLeftShapeLayer = CAShapeLayer()
-    let bgShapeLayer = CAShapeLayer()
-    var timeLeft: TimeInterval?
-    var endTime: Date?
-   // var timeLabel =  UILabel()
-    
-    
-    var timer = Timer()
-    // here you create your basic animation object to animate the strokeEnd
-    let strokeIt = CABasicAnimation(keyPath: "strokeEnd")
-    
+   
+    let shapeLayer = CAShapeLayer()
     override func viewDidLoad() {
         super.viewDidLoad()
+        timeLeftCircle()
+        circleShape()
         timeConverted = Int(time!)
         timeLeft = TimeInterval(timeConverted!)
-        view.backgroundColor = UIColor(white: 0.94, alpha: 1.0)
-        drawBgShape()
-        drawTimeLeftShape()
-        //addTimeLabel()
-        // here you define the fromValue, toValue and duration of your animation
-        strokeIt.fromValue = 0
-        strokeIt.toValue = 1
-        strokeIt.duration = timeLeft!
-        // add the animation to your timeLeftShapeLayer
-        timeLeftShapeLayer.add(strokeIt, forKey: nil)
-        // define the future end time by adding the timeLeft to now Date()
         endTime = Date().addingTimeInterval(timeLeft!)
-       
-    }
-    func drawBgShape() {
-        bgShapeLayer.path = UIBezierPath(arcCenter: CGPoint(x: view.frame.midX , y: view.frame.midY), radius:
-            100, startAngle: -90.degreesToRadians, endAngle: 270.degreesToRadians, clockwise: true).cgPath
-        bgShapeLayer.strokeColor = UIColor.white.cgColor
-        bgShapeLayer.fillColor = UIColor.clear.cgColor
-        bgShapeLayer.lineWidth = 15
-        view.layer.addSublayer(bgShapeLayer)
-    }
-    func drawTimeLeftShape() {
-        timeLeftShapeLayer.path = UIBezierPath(arcCenter: CGPoint(x: view.frame.midX , y: view.frame.midY), radius:
-            100, startAngle: -90.degreesToRadians, endAngle: 270.degreesToRadians, clockwise: true).cgPath
-        timeLeftShapeLayer.strokeColor = UIColor.red.cgColor
-        timeLeftShapeLayer.fillColor = UIColor.clear.cgColor
-        timeLeftShapeLayer.lineWidth = 15
-        view.layer.addSublayer(timeLeftShapeLayer)
     }
     
-//    func addTimeLabel() {
-//        timeLabel = UILabel(frame: CGRect(x: view.frame.midX-50 ,y: view.frame.midY-25, width: 100, height: 50))
-//        timeLabel.textAlignment = .center
-//        timeLabel.text = timeLeft!.time
-//        view.addSubview(timeLabel)
-//    }
+    func timeLeftCircle(){
+        let trackLayer = CAShapeLayer()
+        let circularPath = UIBezierPath(arcCenter: CGPoint(x: view.frame.midX , y: view.frame.midY), radius:
+        100, startAngle: -90.degreesToRadians, endAngle: 270.degreesToRadians, clockwise: true).cgPath
+                     trackLayer.path = circularPath
+                     trackLayer.fillColor = UIColor.clear.cgColor
+                     trackLayer.strokeColor = UIColor.lightGray.cgColor
+                     trackLayer.lineWidth = 10
+                     
+                     trackLayer.lineCap = CAShapeLayerLineCap.round
+               view.layer.addSublayer(trackLayer)
+    }
+    
+    func circleShape(){
+         let circularPath = UIBezierPath(arcCenter: CGPoint(x: view.frame.midX , y: view.frame.midY), radius:
+         100, startAngle: -90.degreesToRadians, endAngle: 270.degreesToRadians, clockwise: true).cgPath
+        shapeLayer.path = circularPath
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = UIColor.purple.cgColor
+        shapeLayer.lineWidth = 10
+        
+        shapeLayer.lineCap = CAShapeLayerLineCap.round
+        shapeLayer.strokeEnd = 0
+        view.layer.addSublayer(shapeLayer)
+    }
     @objc func updateTime() {
         if timeLeft! > 0 {
             timeLeft = endTime?.timeIntervalSinceNow ?? 0
@@ -82,12 +68,24 @@ class PlayScreenViewController: UIViewController {
             timer.invalidate()
         }
     }
-    
+
     @IBAction func playTime(_ sender: Any) {
          timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        handleTap()
     }
     
+    func handleTap(){
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.fromValue = 0
+        basicAnimation.toValue = 1
+        basicAnimation.duration = timeLeft!
+        basicAnimation.fillMode = .forwards
+        basicAnimation.isRemovedOnCompletion = false
+        
+        shapeLayer.add(basicAnimation, forKey: "urSoBasic")
+    }
 }
+
 extension TimeInterval {
     var time: String {
         return String(format:"%02d:%02d", Int(self/60),  Int(ceil(truncatingRemainder(dividingBy: 60))) )
