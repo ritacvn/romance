dbe7a0a4f807d865d6dba7290ee59edaf13900c7
Eliza Carvalho <elizampcarvalho@gmail.com>
2019-11-28 11:21:08 -0400

    Added pop up assets

--- Controller/SettingName/EditNamesViewController.swift
+++ Controller/SettingName/EditNamesViewController.swift
@@ -26,11 +26,14 @@ class EditNamesViewController: UIViewController, UITextFieldDelegate {
     
         self.name1textfield.delegate = self
         self.name2textfield.delegate = self
-        name2Label.isHidden = true
-        name1Label.isHidden = true
+     //  name2Label.isHidden = true
+      //  name1Label.isHidden = true
         popUpView.layer.cornerRadius = 15
-      
+        backgroundImage.layer.cornerRadius = 15
     }
+    
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
     
     @IBAction func saveNamesButton(_ sender: Any) {
         
@@ -41,14 +44,14 @@ class EditNamesViewController: UIViewController, UITextFieldDelegate {
             
             name1textfield.isHidden = true
             name2textfield.isHidden = true
-            name2Label.isHidden = false
-            name1Label.isHidden = false
+           // name2Label.isHidden = false
+           // name1Label.isHidden = false
             
             let name1 = UserDefaults.standard.string(forKey: "initialName01")
             let name2 = UserDefaults.standard.string(forKey: "initialName02")
             
-            name1Label.text = name1
-            name2Label.text = name2
+           // name1Label.text = name1
+           // name2Label.text = name2
         
             self.initialName01 = name1textfield.text!
             self.initialName02 = name2textfield.text!
