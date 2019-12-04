9eb97572560b5ac5c3353143d8cc4890c8aa06d5
Eliza Carvalho <elizampcarvalho@gmail.com>
2019-12-03 22:41:55 -0400

    Bugfix Funny Speech

--- Model/Activities.swift
+++ Model/Activities.swift
@@ -13,9 +13,9 @@ struct Activities {
     let image: String?
  
     static func fetchActivitiesCommunication() -> [Activities]{
-        let timedDiscussion = Activities(title: "Timed Discussion", image: "TimedDiscussion")
-        let funnySpeach = Activities(title: "FunnySpeech", image: "FunnySpeech")
-        let theSentencesActivity = Activities(title: "TimedDiscussion", image: "TheSentencesActivity")
+        let timedDiscussion = Activities(title: "Timed Discussion", image: "atv1_c")
+        let funnySpeach = Activities(title: "FunnySpeech", image: "atv2_c")
+        let theSentencesActivity = Activities(title: "TimedDiscussion", image: "atv3_c")
 
         
         return [timedDiscussion,funnySpeach,theSentencesActivity]
